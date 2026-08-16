8675900a82533a38a1b0e9777942d4ad4d379c8b
Jeffrey Knutsen <jknutsen92@gmail.com>
2026-08-15 23:48:43 -0700

    Updated installer. Tweaked bfg, chaingun ammo drops. Increased armorProtection to 85% and increased damage scaling to 4x

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # /bin/bash
-zip -r ssr.zip pak/*;
-mv ./ssr.zip /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/zzzzzz_smolspacer_rebalanced.pk4
 cp ./autoexec.cfg /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/
+cd pak
+zip -r ssr.zip ./*;
+mv ./ssr.zip /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/zzzzzz_smolspacer_rebalanced.pk4
