74567a7f910a7036cf342a00e0e7d6ba8723c31d
Jeffrey Knutsen <jknutsen92@gmail.com>
2026-08-15 23:07:55 -0700

    Reorganized directory structure, raised max armor to 150

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 # /bin/bash
-zip -r ssr.zip ./*;
+zip -r ssr.zip pak/*;
 mv ./ssr.zip /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/zzzzzz_smolspacer_rebalanced.pk4
+cp ./autoexec.cfg /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/
