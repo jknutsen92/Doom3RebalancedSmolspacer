0cfd082d1e550d7cd5d6e10126594bf2e20a84d4
Jeffrey Knutsen <jknutsen92@gmail.com>
2026-08-22 18:54:01 -0700

    First test values for chainsaw armor drop

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp ./autoexec.cfg /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/
 cd pak
 zip -r ssr.zip ./*;
 mv ./ssr.zip /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/zzzzzz_smolspacer_rebalanced.pk4
-cp ./* /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/smolspacer
+cp -r ./* /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/smolspacer
