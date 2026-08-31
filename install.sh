14323aab4a27004f2df1bf1cba67613b4cbda492
Jeffrey Knutsen <jknutsen92@gmail.com>
2026-08-31 12:18:17 -0700

    Cleaned up files, added new mc_underground map, fixed pistol_flashlight dropping ammo

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp ./autoexec.cfg /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/
 cd pak
 zip -r ssr.zip ./*;
 mv ./ssr.zip /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/base/zzzzzz_smolspacer_rebalanced.pk4
-cp -r ./* /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/smolspacer
+cp -rf ./* /home/jeffrey/.steam/steam/steamapps/common/Doom\ 3/smolspacer
