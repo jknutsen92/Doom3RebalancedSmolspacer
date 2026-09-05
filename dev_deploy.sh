77bdf0275bf3aafa66bc972843d350cf6155c4a2
Jeffrey Knutsen <jknutsen92@gmail.com>
2026-09-04 22:35:49 -0700

    Reduced mixing of imp sight sound effect

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -1,5 +1,5 @@
 # /bin/bash
 doom3_base_dir="/home/jeffrey/.steam/steam/steamapps/common/Doom 3/base/"
 cd pak
-cp -rf ./* "$doom3_base_dir"
+cp -rvu ./* "$doom3_base_dir"
 rm "$doom3_base_dir/zzzzzz_smolspacer_rebalanced.pk4"
